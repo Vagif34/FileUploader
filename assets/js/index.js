1724721beeba0676f6839543c22bc4aa52a8608c
Vagif34 <vagifrg@code.edu.az>
2022-11-01 18:55:01 +0400

    Uploader

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+const uploadIcon = document.querySelector(".upload i");
+const inputFile = document.querySelector(".input");
+
+uploadIcon.addEventListener("click", function () {
+  this.nextElementSibling.click();
+});
+
+inputFile.addEventListener("change", function (e) {
+  const { files } = e.target;
+
+  imageUploader(files);
+});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
